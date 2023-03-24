a971800c2a95bf6f8be3ff4dae2c6ad6f19e904d
Garrett Gregor <garrett.gregor@gmail.com>
2023-03-23 18:07:37 -0600

    Make a node class

---
+++ spec/node_spec.rb
@@ -0,0 +1,29 @@
+require 'rspec'
+require './lib/node'
+
+RSpec.describe Node do
+  it "exists" do
+    node = Node.new("plop")
+    
+    expect(node.data).to eq("plop")
+  end
+  
+  it "has an empty node behind it" do
+    node = Node.new("plop")
+    
+    expect(node.data).to eq("plop")
+    expect(node.next_node).to eq(nil)
+  end
+  
+
+
+end
+
+#   xit "Makes a Node" do
+#     node = Node.new("plop")
+
+#     expect(node).to an_instance_of(Node)
+#     expect(node.sound).to eq("plop")
+#     expect(node.next_node).to eq(nil)
+#   end
+# end
