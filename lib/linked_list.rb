6ec08a0ef1bc8f7c9aa13adbfce7ea3702a66415
Garrett Gregor <garrett.gregor@gmail.com>
2023-03-26 12:08:03 -0600

    Actually finish iteration 1 and string together two nodes data

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -28,10 +28,6 @@ class LinkedList
     # else
     #   last_node(head).next_node = new_node(next_node_data)
     # end
-# If it is empty, assign the new node to the head of the list
-# If it is not empty, find the last_node in the list
-# Assign the last_node's next node to be our new node
-    # self.head = Node.new(next_node_data)
     next_node_data
   end
 
@@ -40,37 +36,76 @@ class LinkedList
   end
 
   def set_tail(set_tail_data)
-    last_node(head).next_node = new_node(set_tail_data)
+    last_node(head).next_node = new_node(set_tail_data) #this will set the last node using the head data and then set the next node to be a new node using the new node method and the set_tail_data provided
   end
   
-  def last_node(pants)
-    return pants if (pants).tail?
-    last_node(pants.next_node)
+  def last_node(last_node_data)
+    return last_node_data if (last_node_data).tail?
+    last_node(last_node_data.next_node)
   end
 
   def new_node(new_node_data)
     Node.new(new_node_data)
   end
 
+  def count_node(node, counter)
+    return counter if node.tail? 
+    # return the counter if the node is the tail using the tail method
+    count_node(node.next_node, counter += 1)
+    # use the count_node method on all nodes.next_nodes to add one to the counter
+  end
+
   def count
-    if head.nil? # if the head of the list is 0, then...
-      0
-    else #otherwise...
-      1
-    end
+    return 0 if empty?
+    # return 0 if the empty method is true
+    count_node(head, 1)
+    # this will use the count_node method we've created using the head as the parameter and setting the counter to one, which will then get one added to the counter every time there is a node.next_node
+
+    ## Previous version to pass the first tests - this only works for one node in a list since there are only two conditions.
+    # if head.nil? # if the head of the list is 0, then...
+    #   0
+    # else #otherwise...
+    #   1
+    # end
   end
 
   def to_string
-    "#{@head.data}"
+    return "" if empty?
+    # this will return a blank string if the empty method returns true
+    return head.data if head.tail?
+    # this will return the head data if the head is the tail, checked using the tail? method which will return true if the head is the tail
+    # return "#{head.data} #{head.next_node.data}" if self.count == 2
+    string_nodes_together(head.data, head.next_node.data)
+
+    ## Iteration one could only take the data from the head to pass the initial tests
+    # "#{@head.data}"
+  end
+
+  # def string_starter
+  #   "#{head.data}"
+  # end
+
+  def string_nodes_together(node_x, node_x_plus_1)
+    return concat(node_x, node_x_plus_1)
+    string_nodes_together(head, concat(node_x, node_x_plus_1))
+  end
+
+
+
+  def concat(node_x, node_x_plus_1)
+    "#{node_x} #{node_x_plus_1}"
+    #this method will put together a node and the node after it
   end
 
 
   def empty?
-    if head != nil # this could be shortened to head.nil?
-      false
-    else
-      true
-    end
+    head.nil?
+    ## Previous Iteration
+    # if head != nil # this could be shortened to head.nil? which would ask the question "Is the head nothing?" to which the answer could only be true/false
+    #   false
+    # else
+    #   true
+    # end
   end
 end
 
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -17,9 +17,8 @@ RSpec.describe LinkedList do
   
   it "can append nodes to the linked list" do
     list = LinkedList.new
+    
     expect(list.head).to be(nil)
-
-    # list.append("doop")
     expect(list.append("doop")).to eq("doop")
   end
   
@@ -65,6 +64,59 @@ RSpec.describe LinkedList do
     list.append("doop")
 
     expect(list.last_node(list.head)).to be(list.head)
+  end
+  
+  it "can append Multiple Nodes" do
+    list = LinkedList.new
+
+    expect(list).to be_an_instance_of(LinkedList)
+    expect(list.head).to eq(nil)
+    
+    list.append("doop")
+    expect(list.head.data).to eq("doop")
+    expect(list.head).to be_an_instance_of(Node)
+    expect(list.head.next_node).to eq(nil)
+    
+    list.append("deep")
+    expect(list.head.next_node.data).to eq("deep")
+  end
+  
+  it "can count multiple Nodes" do
+    list = LinkedList.new
+
+    expect(list).to be_an_instance_of(LinkedList)
+    expect(list.head).to eq(nil)
+    
+    list.append("doop")
+    expect(list.head.data).to eq("doop")
+    expect(list.head).to be_an_instance_of(Node)
+    expect(list.head.next_node).to eq(nil)
+    
+    list.append("deep")
+    expect(list.head.next_node.data).to eq("deep")
+    expect(list.count).to eq(2)
+  end
+  
+  
+  it "return an empty string if the string is empty" do
+    list = LinkedList.new
+
+    expect(list.to_string).to eq("")
+  end
+
+  it "can put one nodes' data to a single string" do
+    list = LinkedList.new
+    list.append("doop")
+
+    expect(list.to_string).to eq("doop")
+  end
+  
+  it "can put two nodes' data to a single string" do
+    list = LinkedList.new
+    list.append("doop")
+    list.append("deep")
+ 
+    expect(list.to_string).to eq("doop deep")
   end
   
   it "Append, All/To String, and Insert (Multiple Nodes)" do
